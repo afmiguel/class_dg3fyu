c039a5e9651d2b27bbdf0d0b0aaa811ef9081ac9
Afonso Miguel <afonso.miguel@pucpr.br>
2025-10-06 16:41:54 -0300

    Update from professor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::thread;
+use std::thread::sleep;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let t1 = thread::spawn(|| {
+        for i in 0..20 {
+            println!("\tI'm a new Thread! Count is {}", i);
+            sleep(Duration::from_secs(1));
+        }
+    });
+
+    for i in 0..10 {
+        println!("I'm the main Thread! Count is {}", i);
+        sleep(Duration::from_secs(1));
+    }
+
+    println!(">> Main Thread is done!");
+    println!(">> It's waiting for the new Thread to finish.");
+    t1.join().unwrap();
 }
